c602995f9cb4443af1d315ea7241c2e5f2945a9a
Adrian Oeyen <oeyenadrian@gmail.com>
2024-06-12 22:55:02 +0200

    Add definitions

--- include/algorithm.h
+++ include/algorithm.h
@@ -1,1 +1,16 @@
 #pragma once
+
+#include <stdlib.h>
+
+typedef struct {
+    long *weight;
+    long *profit; 
+    size_t number_items;
+} KnapsackInput;
+
+typedef struct {
+    size_t *items;
+    size_t amount;
+} Solution;
+
+Solution nemhauser_ullmann(KnapsackInput input);
