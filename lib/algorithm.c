7fd848140182940fd0513ec201d23236ef92b4f3
Adrian Oeyen <oeyenadrian@gmail.com>
2024-06-13 10:41:11 +0200

    Add ParetoSolution struct

---
+++ lib/algorithm.c
@@ -0,0 +1,15 @@
+#include "algorithm.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef struct ParetoSolution ParetoSolution;
+
+struct ParetoSolution {
+    long weight;
+    long profit;
+    size_t prev_item;
+    size_t item_index;
+    size_t prev_set_index;
+};
+
