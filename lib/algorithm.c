11215a35aa07dfc0ae4f1ff6f6491bc7f0a67661
Adrian Oeyen <oeyenadrian@gmail.com>
2024-06-13 10:41:52 +0200

    Add function to generate empty solution

--- lib/algorithm.c
+++ lib/algorithm.c
@@ -13,6 +13,16 @@ struct ParetoSolution {
     size_t prev_set_index;
 };
 
+ParetoSolution empty_solution() {
+    return (ParetoSolution) {
+        .weight = 0,
+            .profit = 0,
+            .prev_item = -1,
+            .item_index = -1,
+            .prev_set_index = -1,
+    };
+}
+
 typedef struct {
     ParetoSolution* solutions;
     size_t amount;
