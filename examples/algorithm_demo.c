37cd3c39aae6321caa98ade780a7c449a67f4a44
Adrian Oeyen <oeyenadrian@gmail.com>
2024-06-13 11:04:08 +0200

    Add simple demo

--- examples/algorithm_demo.c
+++ examples/algorithm_demo.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
 
+#include "algorithm.h"
+
 int main(int argc, char const *argv[])
 {
-    printf("Hello World! \n");
+
+    long profits[] = {1, 5, 6, 7};
+    long weights[] = {2 , 2, 10, 6};
+    long capacity = 12;
+
+    KnapsackInput input = {
+        .capacity = capacity,
+        .profit = profits,
+        .weight = weights,
+        .number_items = 4
+    };
+
+    Solution sol = nemhauser_ullmann(input);
+
+    for(int i = 0;i < sol.amount;i++){
+        printf("%ld ", sol.items[i]);
+    }
+    printf("\n");
+
+    free(sol.items);
+
     return 0;
 }
