be8206fc8c600ffd63bdbd5889d8e80856176f03
Adrian Oeyen <oeyenadrian@gmail.com>
2024-06-13 18:39:41 +0200

    Use data reading functions to read input

--- examples/algorithm_demo.c
+++ examples/algorithm_demo.c
@@ -1,42 +1,34 @@
 #include <stdio.h>
 
 #include "algorithm.h"
+#include "data.h"
 
-int main(int argc, char const *argv[])
-{
-
-    FILE* file = fopen("instances_01_KP/large_scale/knapPI_1_1000_1000_1", "r");
-    if(file == NULL) {
-        fprintf(stderr, "Could not open file! \n");
+int main(int argc, char const* argv []) {
+    if(argc < 2){
+        fprintf(stderr, "Please provide a filepath \n");
         exit(1);
     }
 
-    int amount, capacity;
-    fscanf(file, "%d %d\n", &amount, &capacity);
-
-    KnapsackInput input;
-    input.capacity = capacity;
-    input.number_items = amount;
-    input.profit = malloc(amount * sizeof(long));
-    input.weight = malloc(amount * sizeof(long));
-    for(int i = 0;i < amount;i++){
-        fscanf(file, "%ld %ld\n", input.profit + i, input.weight + i);
+    // TODO: Support directories
+    if(!is_regular_file(argv[1])) {
+        fprintf(stderr, "Currently only direct paths to fils are supported \n");
+        exit(1);
     }
 
-    fclose(file);
+    KnapsackInput input;
+    read_input(argv[1], &input);
 
     printf("Number: %ld, Cap: %ld \n", input.number_items, input.capacity);
 
-
     Solution sol = nemhauser_ullmann(input);
-    for(int i = 0;i < sol.amount;i++){
+    for (int i = 0;i < sol.amount;i++) {
         printf("%ld ", sol.items[i]);
     }
     printf("\n");
 
     long sum = 0;
-    for(int i = 0;i < sol.amount;i++){
-        sum += input.profit[sol.items[i]-1];
+    for (int i = 0;i < sol.amount;i++) {
+        sum += input.profit[sol.items[i] - 1];
     }
 
     printf("Result: %ld\n", sum);
